abd46c6e3518dc6c475d6b0e65bce89312dced63
redukeee <daartemev@edu.hse.ru>
2025-09-18 10:31:24 +0300

    test with hooks

--- expl1.py
+++ expl1.py
@@ -7,15 +7,15 @@ def PtrSort(arr):
     while ptr1 <= ptr2:
         if abs(arr[ptr1]) > abs(arr[ptr2]):
             b[ptr2 - ptr1] = abs(arr[ptr1])
-            ptr1+=1
+            ptr1 += 1
         else:
             b[ptr2 - ptr1] = abs(arr[ptr2])
-            ptr2-=1 
+            ptr2 -= 1
     return b
+
 
 print(PtrSort([-3, -2, 0, 1, 3, 5]))
 print(PtrSort([6]))
 print(PtrSort([-5, -4, -3, -2, 1]))
 print(PtrSort([-5, 0, 3, 3, 5, 10]))
 print(PtrSort([]))
-
